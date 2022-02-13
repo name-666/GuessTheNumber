52f23631c94bdd0f4d0c104fae83dd4cf5dec396
localhost\666 <999localhost@gmail.com>
2022-02-13 16:17:20 +0300

    Угадай число.

---
+++ Game.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+  static  class Game
+    {
+       static public  bool exit = false;
+        static public void game()
+        {
+
+            int dnr = RandomNumber.number();
+
+            Console.WriteLine("Я загадал число.  Попробуй угадать его");
+            while (exit)
+            {
+
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < dnr)
+                {
+                    Console.WriteLine("Больше");
+                }
+                else if (number > dnr)
+                {
+                    Console.WriteLine("Меньше");
+                }
+                else
+                {
+                    Console.WriteLine("Угадал");
+
+
+
+                    Console.WriteLine("Ещё хочешь?");
+
+                    string next = Console.ReadLine();
+                    if (next.ToLower() == "y" || next.ToLower() == "yes")
+                    {
+                        game();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Пока");
+                        exit = false ;
+                    }
+
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ RandomNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    static class RandomNumber
+    {
+        static private readonly Random rnd = new();
+        static private readonly int i = rnd.Next(1, 10);
+      static public int number()
+        {
+            return i;
+        }
+        
+    }
+}
